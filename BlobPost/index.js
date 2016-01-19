06d0eaaee43da67d8da7de076aa0507f34170efd
famasya <abid.famasya@gmail.com>
2016-01-20 00:55:23 +0700

    big image experiment

--- BlobPost/index.js
+++ BlobPost/index.js
@@ -3,13 +3,14 @@
 var fs = require('fs');
 
 var loadBuffer = function(fn){
-  fs.readFile('coral.jpg',function(err,data){
+  fs.readFile('big.jpg',function(err,data){
     if(err) return fn(err,null)
     var encode = data.toString('base64')
     return fn(null,encode)
   })
 }
 
+//big file is not a problem
 loadBuffer(function(err,data){
   var decode = new Buffer(data,'base64');
   fs.writeFile('out.jpg',decode,function(err){
